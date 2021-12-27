a082089d0cfa068d2b42ec03b281799669ca73de
gabormakeev <makeyev@twnty.de>
2021-12-27 20:25:08 +0200

    Finished lesson 6 practice

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'product')]
-    public function index(): Response
-    {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+  #[Route('/product/{product_id}', name: 'product')]
+  public function getProduct($product_id, ManagerRegistry $managerRegistry): Response
+  {
+    $manager = $managerRegistry->getRepository(Product::class);
+    $product = $manager->find($product_id);
+    foreach ($product->getImages() as $image) {
+      var_dump($image);
     }
+    die();
+  }
 }
